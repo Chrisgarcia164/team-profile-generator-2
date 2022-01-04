d31f7414ac0f2b8e45f408ae2242fb4c980eabff
Chrisgarcia164 <91617441+Chrisgarcia164@users.noreply.github.com>
2022-01-04 17:24:47 -0500

    created manager, intern, employee and engineerclasses as well as created initial file structure for index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
