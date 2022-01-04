6617790b4b6ddc77204f9259436a479a90dcdcc8
Chrisgarcia164 <91617441+Chrisgarcia164@users.noreply.github.com>
2022-01-04 18:07:40 -0500

    created prompt to get all info for team

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ inquirer
   .prompt([
     {
       type: "input",
-      message: "What is your name",
+      message: "What is the manager's name",
       name: "firstName",
     },
     {
       type: "input",
-      message: "What is your email",
+      message: "What is the manager's email",
       name: "email",
     },
     {
       type: "input",
-      message: "What is your office number",
+      message: "What is the manager's office number",
       name: "office",
     },
     {
       type: "input",
-      message: "What is your ID?",
+      message: "What is the manager's id?",
       name: "id",
     },
   ])
@@ -53,9 +53,103 @@ const addNewMember = () => {
       switch (answers.add) {
         case true:
           console.log("new team ");
+          createNewMember();
           break;
         case false:
-          console.log("no ");
+          console.log("no");
       }
+    });
+};
+const createNewMember = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Please choose the employee's role",
+        name: "role",
+        choices: ["Engineer", "Intern"],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.role) {
+        case "Engineer":
+          console.log("engineer chosen");
+          engineerPrompt();
+          break;
+        case "Intern":
+          console.log("Intern chosen");
+          internPrompt();
+          break;
+      }
+    });
+};
+const internPrompt = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "what is the intern's name",
+        name: "firstName",
+      },
+      {
+        type: "input",
+        message: "what is the intern's email",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "what is the intern's id",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "what school does the intern attend",
+        name: "school",
+      },
+    ])
+    .then((answers) => {
+      let name = answers.firstName;
+      let email = answers.email;
+      let school = answers.school;
+      let id = answers.id;
+      const intern = new Intern(name, id, email, school);
+      console.log(intern);
+      team.push(intern);
+      addNewMember();
+    });
+};
+const engineerPrompt = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "what is the engineer's engineer's name",
+        name: "firstName",
+      },
+      {
+        type: "input",
+        message: "what is the engineer's id",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "what is the engineer's id",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "what is the engineer's github",
+        name: "github",
+      },
+    ])
+    .then((answers) => {
+      let name = answers.firstName;
+      let email = answers.email;
+      let github = answers.github;
+      let id = answers.id;
+      const engineer = new Engineer(name, id, email, github);
+      console.log(engineer);
+      team.push(engineer);
+      addNewMember();
     });
 };
